ef30e1b6829a589a1fbaeb8e54c27dd5d815362e
notU <notyoudev@gmail.com>
2020-03-10 21:54:57 -0400

    TODO Need to find out the correct rate-limit refresh. [rewrite (2/3)]

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # tripMiner
-A simple hack.chat bot to mine tripcodes
-## Credit
-Cereals => Large amounts of bug fixing
+A simple [hack.chat](https://hack.chat) bot to mine tripcodes
+
+# How to use
+Config the [config.json](./config.json) with relevant information
+
+Run [runMe.js](./runMe.js)
--- src/brain.js
+++ src/brain.js
@@ -2,30 +2,38 @@ const web = require('ws');
 const fs = require('fs');
 
 module.exports.mine = function (d) {
-  let ws = new web('wss://hack.chat/chat-ws');
+  const ws = new web('wss://hack.chat/chat-ws');
 
   let name = d.name;
   let limit = d.limit;
   let channel = d.channel;
 
-  ws.on("open", function() {
+  ws.on('open', function() {
     console.log(`channel: ${channel}\nname: ${name}`); // debugging
-    var pass;
+    let pass = "";
+    let rateLimited = false;
+
     ws.on('message', function(_d) {
       let p = JSON.parse(_d);
-      if (p.cmd !== 'chat') {return}
-      let trip = p.trip;
-      fs.appendFile('./log.txt', `\ntrip: ${trip} | password: ${pass}`, (err) => {
-        if (err) throw err;
-        console.log(trip+":"+pass); // debugging
-      });
+      if (p.trip) {
+        let trip = p.trip;
+        fs.appendFile('./log.txt', `\ntrip: ${trip} | password: ${pass}`, (err) => {
+          if (err) throw err;
+          console.log(trip+":"+pass); // debugging
+        });
+      } else if (p.cmd == 'warn') {
+        console.log(p.text);
+        rateLimited = true;
+        setTimeout(function(){rateLimited = false}, 30000) // rate limit check refresh
+      }
     });
+
     for (let i=0; i < limit; i++) {
       function c() {
+        if (rateLimited) {return}
         pass = Math.random().toString(36).substr(2, 8);
         ws.send(JSON.stringify({ cmd:"join", nick:(name+"#"+pass) }));
-        ws.send(JSON.stringify({ cmd:"chat", text: Math.random().toString(36).substr(2, 4) }));
-        ws.send(JSON.stringify({ cmd:"disconnect" }));
+        ws.send(JSON.stringify({ cmd:"chat", text: "I'm miningfortrips! github: github.com/realnotU/tripMiner" }));
       }
       setTimeout(c, i * 5000);
     }
